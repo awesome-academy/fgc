7204b896f6d0d6ac84bd4223c91d8e65357d798a
Nguyen Quang Anh Tai <nguyen.quang.anh.tai@sun-asterisk.com>
2019-10-07 09:43:48 +0700

    Show topic to User

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,10 @@
 class Topic < ApplicationRecord
   has_many :posts
   has_many :sub_topics, class_name: Topic.name,
-                      foreign_key: :parent_id
+                      foreign_key: :topic_id
 
   belongs_to :parent_topic, class_name: Topic.name, optional: true
+  scope :root_topics, ->{where topic_id: nil}
+  scope :select_id_content, ->{select :id, :content}
+  scope :alphabet_content, ->{order :content}
 end
